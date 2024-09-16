4ed4403b515de3ae276f45972e789813e2339dc1
Maxim Datskiy <datskiumax@gmail.com>
2024-09-16 00:29:46 -0700

    complete section 7
    including boss challenge
    also added a nice animation

--- Xylophone-iOS13/Xylophone/ViewController.swift
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -19,12 +19,24 @@ class ViewController: UIViewController {
 
     
     @IBAction func keyPressed(_ sender: UIButton) {
+        print("Start")
+        UIView.animate(withDuration: 0.2) {
+            sender.layer.opacity = 0.5
+        }
+        
         playSound(sender.currentTitle!)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            UIView.animate(withDuration: 0.2) {
+                sender.layer.opacity = 1
+            }
+            print("End")
+        }
     }
 
     
-    func playSound(_ note: String) {
-        guard let url = Bundle.main.url(forResource: note, withExtension: "wav") else { return }
+    func playSound(_ sound: String) {
+        guard let url = Bundle.main.url(forResource: sound, withExtension: "wav") else { return }
         
         do {
             try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
