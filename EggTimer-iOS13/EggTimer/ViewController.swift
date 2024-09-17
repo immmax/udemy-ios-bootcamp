d438285ea774eff81f489b643cc830db27e6829a
Maxim Datskiy <datskiumax@gmail.com>
2024-09-17 00:38:43 -0700

    complete lesson 81

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle
+        
+        if hardness == "Soft" {
+            print("\(softTime) minutes")
+        } else if hardness == "Medium" {
+            print("\(mediumTime) minutes")
+        } else {
+            print("\(hardTime) minutes")
+        }
+    }
+    
 }
